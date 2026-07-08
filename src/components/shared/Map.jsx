063c7453fc454ff8b23c165d3f17b237e29f6311
Ahmed Ibrahim Farouk <ahmed2009ibrahim1983@gmail.com>
2026-07-08 17:52:33 +0300

    feat: add reusable map component

---
+++ src/components/shared/Map.jsx
@@ -0,0 +1,43 @@
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+
+export default function Map({
+    center,
+    markerPosition,
+    popupContent,
+    className = "h-[400px] w-full",
+}) {
+    return (
+        <MapContainer
+            center={center}
+            zoom={16}
+            scrollWheelZoom={false}
+            className={className}
+        >
+            <TileLayer
+                attribution="&copy; OpenStreetMap contributors"
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Marker position={markerPosition}>
+                <Popup>{popupContent}</Popup>
+            </Marker>
+        </MapContainer>
+    );
+}
+
+
+// You can use this component like this
+
+{/* 
+
+<Map
+    center={[30.0444, 31.2357]}
+    markerPosition={[30.0444, 31.2357]}
+    popupContent={
+      <>
+        <h3 className="font-bold">Dr Ahmed</h3>
+        <p>Dentist</p>
+      </>
+  }
+/> 
+*/}
