4777f3114a2ccdc03bf782678ea8305d999a2810
MahmoudRafaat20 <mahmoudrafaatmahmoud3@gmail.com>
2026-07-08 17:13:16 +0300

    booking page ui

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter, RouterProvider, Navigate } from "react-router-dom"
 import { SignInPage } from "@/features/Auth/pages/SignInPage";
 import { SignUpPage } from "@/features/Auth/pages/SignUpPage";
 import { VerifyOtpPage } from "@/features/Auth/pages/VerifyOtpPage";
+import Booking from "@/features/Booking/Booking";
 
 
 export const authRoutes = [
@@ -14,6 +15,7 @@ const router = createBrowserRouter([
   { path: "/", element: <Navigate to="/login" replace /> },
   ...authRoutes,
   // ...other routes 
+  {path:"/booking", element:<Booking/>},
 ]);
 
 export function AppRouter() {
